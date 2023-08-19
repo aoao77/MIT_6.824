be1d29116677cb6c1a14cdf572f4b43f4ce1ba7e
yangjinao <yangjinao@haomozhixing.onaliyun.com>
2023-08-19 16:53:42 +0800

    MapReduce v1

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,154 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"time"
+)
 
+const (
+	task_type_undefined = iota
+	task_type_map
+	task_type_reduce
+)
+
+// const (
+// 	work_state_idle = iota
+// 	work_state_processing
+// 	work_state_completed
+// 	work_state_failed
+// )
+
+const (
+	file_state_created = iota
+	file_state_allocated
+	file_state_finished
+)
+
+type Task struct {
+	Task_no      int
+	Task_type    int
+	File_name_in string
+	Job_nMap     int
+	Job_nReduce  int
+	Condition    bool
+}
 
 type Coordinator struct {
 	// Your definitions here.
-
+	nMap               int
+	nReduce            int
+	map_timer          []int64
+	reduce_timer       []int64
+	files_array        []string
+	files_map_array    []uint8
+	files_reduce_array []uint8
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
+func (c *Coordinator) Ordinator_begin(args *Task, reply *Task) error {
+	//Time out
+	timeNow := time.Now().Unix()
+	for i := 0; i < c.nMap; i++ {
+		if c.files_map_array[i] == file_state_allocated && timeNow-c.map_timer[i] > 10 {
+			c.files_map_array[i] = file_state_created
+			c.map_timer[i] = 0
+		}
+	}
+	for i := 0; i < c.nReduce; i++ {
+		if c.files_reduce_array[i] == file_state_allocated && timeNow-c.reduce_timer[i] > 10 {
+			c.files_reduce_array[i] = file_state_created
+			c.reduce_timer[i] = 0
+		}
+	}
+	//fork task
+	if args.Task_type == task_type_undefined {
+		task_allocated := false
+		for i, file_state := range c.files_map_array {
+			if file_state == file_state_created {
+				reply.Task_no = i
+				reply.Task_type = task_type_map
+				reply.File_name_in = c.files_array[i]
+				reply.Job_nMap = c.nMap
+				reply.Job_nReduce = c.nReduce
+				reply.Condition = true
 
-//
+				c.files_map_array[i] = file_state_allocated
+				c.map_timer[i] = time.Now().Unix()
+				task_allocated = true
+				break
+			}
+		}
+
+		if !task_allocated {
+			for i, file_state := range c.files_reduce_array {
+				if file_state == file_state_created {
+					reply.Task_no = i
+					reply.Task_type = task_type_reduce
+					reply.File_name_in = ""
+					reply.Job_nMap = c.nMap
+					reply.Job_nReduce = c.nReduce
+					reply.Condition = false
+
+					condition_flag := true
+					for _, val := range c.files_map_array {
+						if val != file_state_finished {
+							condition_flag = false
+							break
+						}
+					}
+					if condition_flag {
+						reply.Condition = true
+						c.files_reduce_array[i] = file_state_allocated
+						c.reduce_timer[i] = time.Now().Unix()
+					}
+					break
+				}
+			}
+		}
+	} else if args.Task_type == task_type_reduce {
+		if c.files_reduce_array[args.Task_no] == file_state_created {
+			condition_flag := true
+			for _, val := range c.files_map_array {
+				if val != file_state_finished {
+					condition_flag = false
+					break
+				}
+			}
+			if condition_flag {
+				reply.Condition = true
+				c.files_reduce_array[args.Task_no] = file_state_allocated
+				c.reduce_timer[args.Task_no] = time.Now().Unix()
+			}
+		}
+	}
+	return nil
+}
+
+func (c *Coordinator) Ordinator_end(args *Task, reply *Task) error {
+	if args.Task_type == task_type_map {
+		c.files_map_array[args.Task_no] = file_state_finished
+	} else if args.Task_type == task_type_reduce {
+		c.files_reduce_array[args.Task_no] = file_state_finished
+	}
+	reply.Condition = true
+
+	return nil
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,30 +162,47 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
-	ret := false
+	ret := true
 
-	// Your code here.
-
+	//reduce
+	for i := 0; i < len(c.files_reduce_array); i++ {
+		if c.files_reduce_array[i] != file_state_finished {
+			ret = false
+			break
+		}
+	}
 
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
-
-	// Your code here.
-
-
+	//input argument
+	c.nMap = len(files)
+	c.nReduce = nReduce
+	c.files_array = files
+	c.files_map_array = make([]uint8, len(files))
+	for i := 0; i < len(c.files_map_array); i++ {
+		c.files_map_array[i] = file_state_created
+	}
+	c.files_reduce_array = make([]uint8, nReduce)
+	for i := 0; i < len(c.files_reduce_array); i++ {
+		c.files_reduce_array[i] = file_state_created
+	}
+	c.map_timer = make([]int64, len(files))
+	for i := 0; i < len(c.map_timer); i++ {
+		c.map_timer[i] = 0
+	}
+	c.reduce_timer = make([]int64, nReduce)
+	for i := 0; i < len(c.reduce_timer); i++ {
+		c.reduce_timer[i] = 0
+	}
 	c.server()
 	return &c
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,48 +1,276 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"fmt"
+	"hash/fnv"
+	"io"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"time"
 
+	// "io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
 
-//
+	"encoding/json"
+)
+
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
-// main/mrworker.go calls this function.
-//
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
-
-	// Your worker implementation here.
-
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
-
+// if exist, err := file_exist(path); exist == true && err == nil {
+func file_exist(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
 }
 
-//
+func files_delete(path string) error {
+	files, err := filepath.Glob(path)
+	if err != nil {
+		log.Fatalf("cannot glob %s", path)
+	}
+	for _, f := range files {
+		if err := os.Remove(f); err != nil {
+			log.Fatalf("cannot remove %s", f)
+		}
+	}
+	return err
+}
+
+func task_exec_mapf(work_task *Task, mapf func(string, string) []KeyValue) {
+	//delete temp file
+	pwd, _ := os.Getwd()
+	temp_name := fmt.Sprintf("mr-%d-*-temp.*", work_task.Task_no)
+	files_delete(temp_name)
+
+	//read file
+	file, err := os.Open(work_task.File_name_in)
+	if err != nil {
+		log.Fatalf("cannot open %v", work_task.File_name_in)
+	}
+	content, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", work_task.File_name_in)
+	}
+	file.Close()
+	//func map
+	intermediate := []KeyValue{}
+	kva := mapf(work_task.File_name_in, string(content))
+	intermediate = append(intermediate, kva...)
+	//sort
+	sort.Sort(ByKey(intermediate))
+	//create temp file
+	ofiles_temp := make([]*os.File, work_task.Job_nReduce)
+	encs_temp := make([]*json.Encoder, work_task.Job_nReduce)
+	for i := 0; i < work_task.Job_nReduce; i++ {
+		oname := fmt.Sprintf("mr-%d-%d-temp.*", work_task.Task_no, i)
+		ofile, err := os.CreateTemp(pwd, oname)
+		if err != nil {
+			log.Fatalf("cannot CreateTemp %v", oname)
+		}
+		//defer remove
+		defer os.Remove(oname)
+		ofiles_temp[i] = ofile
+		encs_temp[i] = json.NewEncoder(ofile)
+	}
+
+	//merge
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		sum := 0
+		for k := i; k < j; k++ {
+			conv_int, err := strconv.Atoi(intermediate[k].Value)
+			if err != nil {
+				log.Fatalf("strconv failed : %s", intermediate[k].Value)
+			}
+			sum += conv_int
+		}
+		intermediate[i].Value = strconv.Itoa(sum)
+		//output
+		reduce_no := ihash(intermediate[i].Key) % work_task.Job_nReduce
+		err := encs_temp[reduce_no].Encode(&intermediate[i])
+		if err != nil {
+			log.Fatalf("Encode failed : %s , %s ", intermediate[i].Key, intermediate[i].Value)
+		}
+
+		i = j
+	}
+
+	//atomic rename file
+	for i := 0; i < work_task.Job_nReduce; i++ {
+		oname := fmt.Sprintf("mr-%d-%d", work_task.Task_no, i)
+		if exist, _ := file_exist(oname); !exist {
+			err := os.Rename(ofiles_temp[i].Name(), oname)
+			if err != nil {
+				log.Fatalf("cannot Rename %v to %v", ofiles_temp[i].Name(), oname)
+			}
+		}
+	}
+}
+
+func task_exec_reducef(work_task *Task, reducef func(string, []string) string) {
+	//delete temp_file
+	pwd, _ := os.Getwd()
+	temp_name := fmt.Sprintf("mr-out-%d-temp.*", work_task.Task_no)
+	files_delete(temp_name)
+
+	//create temp_file
+	oname_temp := fmt.Sprintf("mr-out-%d-temp.*", work_task.Task_no)
+	ofile_temp, err := os.CreateTemp(pwd, oname_temp)
+	if err != nil {
+		log.Fatalf("cannot CreateTemp %v", oname_temp)
+	}
+	//defer remove
+	defer os.Remove(oname_temp)
+
+	intermediate := []KeyValue{}
+	//open intermediate file
+	for i := 0; i < work_task.Job_nMap; i++ {
+		file_name_in := fmt.Sprintf("mr-%d-%d", i, work_task.Task_no)
+		file, err := os.Open(file_name_in)
+		if err != nil {
+			log.Fatalf("cannot open %v", work_task.File_name_in)
+		}
+
+		//read intermediate file
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			intermediate = append(intermediate, kv)
+		}
+		//close
+		file.Close()
+	}
+	//sort
+	sort.Sort(ByKey(intermediate))
+
+	//split
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		sum := 0
+		for k := i; k < j; k++ {
+			conv_int, err := strconv.Atoi(intermediate[k].Value)
+			if err != nil {
+				log.Fatalf("strconv failed : %s", intermediate[k].Value)
+			}
+			sum += conv_int
+		}
+		values_one := []string{}
+		for k := 0; k < sum; k++ {
+			values_one = append(values_one, "1")
+		}
+
+		output := reducef(intermediate[i].Key, values_one)
+
+		fmt.Fprintf(ofile_temp, "%v %v\n", intermediate[i].Key, output)
+
+		i = j
+	}
+	//atomic rename file
+	oname := fmt.Sprintf("mr-out-%d", work_task.Task_no)
+	if exist, _ := file_exist(oname); !exist {
+		err := os.Rename(ofile_temp.Name(), oname)
+		if err != nil {
+			log.Fatalf("cannot Rename %v to %v", ofile_temp.Name(), oname)
+		}
+	}
+}
+
+// main/mrworker.go calls this function.
+func Worker(mapf func(string, string) []KeyValue,
+	reducef func(string, []string) string) {
+	// defination
+	work_task := Task{0, task_type_undefined, "", 0, 0, false}
+
+	for work_task.Task_type == task_type_undefined {
+		if ret := CallTaskBegin(&work_task); !ret {
+			log.Fatalf("CallTaskBegin failed")
+		}
+		time.Sleep(time.Second)
+	}
+
+	if work_task.Task_type == task_type_map {
+		task_exec_mapf(&work_task, mapf)
+	} else if work_task.Task_type == task_type_reduce {
+		for !work_task.Condition {
+			if ret := CallTaskBegin(&work_task); !ret {
+				log.Fatalf("CallTaskBegin failed")
+			}
+			time.Sleep(time.Second)
+		}
+		task_exec_reducef(&work_task, reducef)
+	}
+
+	work_task.Condition = false
+	for !work_task.Condition {
+		if ret := CallTaskEnd(&work_task); !ret {
+			log.Fatalf("CallTaskEnd failed")
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func CallTaskBegin(work_task *Task) bool {
+	ok := call("Coordinator.Ordinator_begin", &work_task, &work_task)
+	if ok {
+		fmt.Printf("call TaskBegin success!\n")
+	} else {
+		fmt.Printf("call TaskBegin failed!\n")
+	}
+	return ok
+}
+
+func CallTaskEnd(work_task *Task) bool {
+	ok := call("Coordinator.Ordinator_end", &work_task, &work_task)
+	if ok {
+		fmt.Printf("call TaskEnd success!\n")
+	} else {
+		fmt.Printf("call TaskEnd failed!\n")
+	}
+	return ok
+}
+
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +295,9 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
